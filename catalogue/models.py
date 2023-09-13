dbb458b8d2791655f3a861e7f1cbf2d1bbcd9560
psharma1984 <psharma.cad@gmail.com>
2023-09-13 16:25:17 -0700

    created models author, genre, book

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    genre = models.ManyToManyField(Genre, related_name="books")
+
+    class Meta:
+        ordering = ["name"]
