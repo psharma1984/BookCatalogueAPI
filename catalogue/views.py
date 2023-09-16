85bc8727ebeaceb6540c4f0366240c6c945e5510
psharma1984 <psharma.cad@gmail.com>
2023-09-16 15:22:48 -0700

    added booklist view

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Book
+from .BookSerializer import BookSerializer
 
 # Create your views here.
+
+
+class BookList(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
