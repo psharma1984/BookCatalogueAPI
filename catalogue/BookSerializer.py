eb74f268967e3fe139f35b51d24dfd69ad11567a
psharma1984 <psharma.cad@gmail.com>
2023-09-16 18:50:21 -0700

    added post methods for all models

--- catalogue/BookSerializer.py
+++ catalogue/BookSerializer.py
@@ -3,18 +3,32 @@ from catalogue.models import Book, Genre, Author
 
 
 class BookSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        genres_data = validated_data.pop("genres", None)
+        book = Book.objects.create(**validated_data)
+        for genre_data in genres_data:
+            book.genres.add(genre_data)
+            book.save()
+        return book
+
     class Meta:
         model = Book
         fields = ["name", "price", "description", "author", "genres"]
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
     class Meta:
         model = Genre
         fields = ["name", "id"]
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
+
     class Meta:
         model = Author
         fields = ["name", "age", "id"]
--- catalogue/views.py
+++ catalogue/views.py
@@ -28,9 +28,25 @@ class AuthorList(APIView):
         serializer = AuthorSerializer(authors, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class GenreList(APIView):
     def get(self, request):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
